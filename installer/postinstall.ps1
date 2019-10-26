a01209df1d1bdc9d2f9dad511f2604637e00a3ed
pdobrzanski <speedcm@myactv.net>
2019-10-26 10:23:33 -0400

    initial

--- installer/postinstall.ps1
+++ installer/postinstall.ps1
@@ -1,2 +1,4 @@
-New-Item c:/cfn/postinstall.txt
+New-Item c:/cfn/postinstall-start.txt
 Invoke-Expression -Command:"icacls C:/inetpub/AspNetCoreWebApps/winged-keys-app /grant 'IIS AppPool\DefaultAppPool:(OI)(CI)M'"
+Invoke-Expression -Command:"icacls C:/inetpub/AspNetCoreWebApps/winged-keys-app"
+New-Item c:/cfn/postinstall-end.txt
