8d5279ac4703bd0769970ac06d6277bb39b59b53
pdobrzanski <speedcm@myactv.net>
2019-10-28 21:28:49 -0400

    initial

--- installer/scripts/postinstall.ps1
+++ installer/scripts/postinstall.ps1
@@ -14,6 +14,11 @@ Invoke-Expression -Command:"icacls C:/inetpub/AspNetCoreWebApps/winged-keys-app 
 Invoke-Expression -Command:"icacls C:/inetpub/AspNetCoreWebApps/winged-keys-app"
 
 #
+# Copy entity framework dll to web root
+#
+Copy-Item -force C:/inetpub/AspNetCoreWebApps/winged-keys-app/installer/lib/ef.dll C:/inetpub/AspNetCoreWebApps/winged-keys-app/ef.dll
+
+#
 # Touch token file for post deploy end (for debugging purposes only)
 #
 fc > c:/cfn/postinstall-end.txt
