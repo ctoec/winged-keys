b6645de91a58ec5b5d0d27be3b21f4b0e93976ec
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-11-14 15:52:35 +0000

    Add database backing
    Follows the IS EF tutorial and removes hardcoded config values

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,17 @@
-echo "starting openid provider server..."
-dotnet watch run --urls "https://0.0.0.0:5050"
+echo "installing dotnet ef utility..."
+until which dotnet-ef || dotnet tool install --global dotnet-ef --version 3.0.0; do
+    sleep 1
+done
+
+echo "waiting for configuration database..."
+until dotnet ef -v database update --context ConfigurationDbContext; do
+    sleep 1
+done
+
+echo "waiting for persisted grant database..."
+until dotnet ef -v database update --context PersistedGrantDbContext; do
+    sleep 1
+done
+
+echo "starting winged keys..."
+dotnet watch run --urls "https://0.0.0.0:5050" -- --context ConfigurationDbContext
--- src/WingedKeys/Config.cs
+++ src/WingedKeys/Config.cs
@@ -7,16 +7,25 @@
 using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
 
 namespace WingedKeys
 {
-  public static class Config
-  {
-	public static List<TestUser> GetUsers()
+	public class Config
 	{
-	  return new List<TestUser>
+		private readonly IConfiguration Configuration;
+
+		public Config(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public List<TestUser> GetUsers()
+		{
+			return new List<TestUser>
 			{
 				new TestUser
 				{
@@ -33,61 +42,66 @@ namespace WingedKeys
 					}
 				}
 			};
-	}
+		}
 
-	public static IEnumerable<IdentityResource> GetIdentityResources()
-	{
-	  return new List<IdentityResource>
+		public IEnumerable<IdentityResource> GetIdentityResources()
+		{
+			return new List<IdentityResource>
 			{
 				new IdentityResources.OpenId(),
 				new IdentityResources.Profile(),
 			};
-	}
+		}
 
-	// http://docs.identityserver.io/en/latest/topics/resources.html#defining-api-resources
-	public static IEnumerable<ApiResource> GetApis()
-	{
-	  return new List<ApiResource>
+		// http://docs.identityserver.io/en/latest/topics/resources.html#defining-api-resources
+		public IEnumerable<ApiResource> GetApis()
+		{
+			return new List<ApiResource>
 			{
 				new ApiResource
-            {
-                Name = "hedwig_backend",
+				{
+					Name = "hedwig_backend",
 
-                Description = "Hedwig API",
+					Description = "Hedwig API",
 
-                // include the following using claims in access token (in addition to subject id)
-                UserClaims = { "role" },
+					// include the following using claims in access token (in addition to subject id)
+					UserClaims = { "role" },
 
-								Scopes =
-                {
-                    new Scope()
-                    {
-                        Name = "hedwig_backend",
-                        DisplayName = "Full access to Hedwig API",
-                        UserClaims = new [] { "role", "allowed_apps" }
-                    }
-                }
-            }
+					Scopes =
+					{
+						new Scope()
+						{
+							Name = "hedwig_backend",
+							DisplayName = "Full access to Hedwig API",
+							UserClaims = new [] { "role", "allowed_apps" }
+						}
+					}
+				}
 			};
-	}
+		}
 
-	public static IEnumerable<Client> GetClients()
-	{
-	  return new List<Client>
+		public IEnumerable<Client> GetClients()
+		{
+			var redirectUris = Configuration.GetValue<string>("RedirectURIs");
+			var postLogoutRedirectUris = Configuration.GetValue<string>("PostLogoutRedirectURIs");
+			var allowedCorsOrigins = Configuration.GetValue<string>("AllowedCorsOrigins");
+			var accessTokenLifetime = Configuration.GetValue<string>("AccessTokenLifetime");
+
+			return new List<Client>
 			{
 				// Hedwig Client
-        new Client
+				new Client
 				{
 					ClientId = "hedwig",
 					ClientName = "Hedwig Client",
 					AllowedGrantTypes = GrantTypes.Code,
 					RequireClientSecret = false,
 
-					RedirectUris =           { "https://localhost:5001/login/callback" },
-					PostLogoutRedirectUris = { "https://localhost:5001" },
-					AllowedCorsOrigins =     { "https://localhost:5001" },
+					RedirectUris =           { redirectUris },
+					PostLogoutRedirectUris = { postLogoutRedirectUris },
+					AllowedCorsOrigins =     { allowedCorsOrigins },
 
-					AccessTokenLifetime = 360000, // 100 hours
+					AccessTokenLifetime = Int32.Parse(accessTokenLifetime),
 
 					AllowedScopes =
 					{
@@ -99,6 +113,6 @@ namespace WingedKeys
 					AllowOfflineAccess = true
 				}
 			};
+		}
 	}
-  }
 }
