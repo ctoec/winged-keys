e6064e8e8c7993f7ace96673cfcddcd60a9c9378
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-11-14 12:49:17 -0500

    Change database initalization order

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@ until which dotnet-ef || dotnet tool install --global dotnet-ef --version 3.0.0;
     sleep 1
 done
 
-echo "waiting for configuration database..."
-until dotnet ef -v database update --context ConfigurationDbContext; do
+echo "waiting for persisted grant database..."
+until dotnet ef -v database update --context PersistedGrantDbContext; do
     sleep 1
 done
 
-echo "waiting for persisted grant database..."
-until dotnet ef -v database update --context PersistedGrantDbContext; do
+echo "waiting for configuration database..."
+until dotnet ef -v database update --context ConfigurationDbContext; do
     sleep 1
 done
 
