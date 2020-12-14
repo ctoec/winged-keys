b18d47d22389575f33ade38ca001f029219f9116
Jordan Guinn <jordan@skylight.digital>
2020-12-13 21:42:02 -0800

    ahhhh, well that makes sense

--- src/WingedKeys/Models/ApplicationUser.cs
+++ src/WingedKeys/Models/ApplicationUser.cs
@@ -1,8 +1,17 @@
 using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace WingedKeys.Models
 {
 	public class ApplicationUser : IdentityUser
 	{
+		/// <summary>
+		/// Gets or sets the last login timestamp for the given user.
+		/// </summary>
+		public virtual DateTimeOffset? LastLogin
+		{
+			get;
+			set;
+		}
 	}
 }
