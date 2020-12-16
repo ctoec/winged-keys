07d0c4e708688a3e3faed5c92ea7c479989998ca
Jordan Guinn <jordan@skylight.digital>
2020-12-16 09:29:46 -0800

    don't need this anymore

--- src/WingedKeys/Models/ApplicationUser.cs
+++ src/WingedKeys/Models/ApplicationUser.cs
@@ -5,13 +5,5 @@ namespace WingedKeys.Models
 {
 	public class ApplicationUser : IdentityUser
 	{
-		/// <summary>
-		/// Gets or sets the last login timestamp for the given user.
-		/// </summary>
-		public virtual DateTimeOffset? LastLogin
-		{
-			get;
-			set;
-		}
 	}
 }
